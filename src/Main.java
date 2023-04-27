6f4a67f2a6ab14627d3d65bc31023a676c587ee8
Szymon Guzik <szguzik@wsb.gda.pl>
2023-04-27 16:02:54 +0200

    zmiana nazwy planety na ziemia

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Pies burek = new Pies();
-        burek.planet = "Merkury";
+        burek.planet = "Ziemia";
         System.out.println(burek.planet);
     }
 }
