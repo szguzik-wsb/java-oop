ca76d978fb6a926b61975f12c3d5226b9b81ead8
Szymon Guzik <szguzik@wsb.gda.pl>
2023-04-27 15:38:53 +0200

    wstep do programowania obiektowego

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Pies burek = new Pies();
+        burek.planet = "Ziemia";
+        System.out.println(burek.planet);
+    }
+}
+class Pies extends Animals {
+    public static String voice(String voice) {
+        return voice;
+    }
+}
+class Animals extends Planet {
+    public String name;
+    public int age;
+    private String zoo;
+}
+
+class Planet {
+   public String planet;
+}
---
+++ src/Main2.java
@@ -0,0 +1,17 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Pies2 burek = new Pies2("Burek", 2);
+
+        System.out.println(burek.name);
+        System.out.println(burek.age);
+    }
+}
+class Pies2 {
+    public String name;
+    public int age;
+    public Pies2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
