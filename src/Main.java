688f0084197e1d30288f99443546f8cd6748759a
Szymon Guzik <szguzik@wsb.gda.pl>
2023-04-27 15:59:27 +0200

    zmiana planety

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Pies burek = new Pies();
-        burek.planet = "Mars";
+        burek.planet = "Merkury";
         System.out.println(burek.planet);
     }
 }
